ad7efba822bf710cd89709320fc6bcfe037e7e8c
Angel <angel.mendez.7985@gmail.com>
2025-06-15 03:25:02 -0600

    Se modifica JS

--- materias.js
+++ materias.js
@@ -1,1 +1,24 @@
-Bienvenido
+const materias = [];
+
+function agregarMateria() {
+  const input = document.getElementById('materiaInput');
+  if (input.value.trim()) {
+    materias.push(input.value.trim());
+    mostrarMaterias();
+    input.value = '';
+  }
+}
+
+function mostrarMaterias() {
+  const ul = document.getElementById('materiasList');
+  ul.innerHTML = '';
+  materias.forEach((m, i) => {
+    ul.innerHTML += `<li>${m} <button onclick="eliminarMateria(${i})">Eliminar</button></li>`;
+  });
+}
+
+function eliminarMateria(index) {
+  materias.splice(index, 1);
+  mostrarMaterias();
+}
+
