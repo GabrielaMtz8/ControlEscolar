e6d0e20f2a2a310b4e0797da30b0079afc84753a
Angel <angel.mendez.7985@gmail.com>
2025-06-15 03:36:04 -0600

    Se modifico horario profesores js

--- horprof.js
+++ horprof.js
@@ -1,1 +1,24 @@
-De profesores
+const horarios = [];
+
+function agregarHorario() {
+  const input = document.getElementById('horarioInput');
+  if (input.value.trim()) {
+    horarios.push(input.value.trim());
+    mostrarHorarios();
+    input.value = '';
+  }
+}
+
+function mostrarHorarios() {
+  const ul = document.getElementById('horariosList');
+  ul.innerHTML = '';
+  horarios.forEach((h, i) => {
+    ul.innerHTML += `<li>${h} <button onclick="eliminarHorario(${i})">Eliminar</button></li>`;
+  });
+}
+
+function eliminarHorario(index) {
+  horarios.splice(index, 1);
+  mostrarHorarios();
+}
+
