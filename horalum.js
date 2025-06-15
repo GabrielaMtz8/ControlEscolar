485c1b1239d9c892178d3f4ee5bb4ce650e9f58f
Angel <angel.mendez.7985@gmail.com>
2025-06-15 03:41:28 -0600

    Se modifico horario alumnos js

--- horalum.js
+++ horalum.js
@@ -1,1 +1,24 @@
-Js
+const alumnos = [];
+
+function agregarAlumno() {
+  const input = document.getElementById('alumnoInput');
+  if (input.value.trim()) {
+    alumnos.push(input.value.trim());
+    mostrarAlumnos();
+    input.value = '';
+  }
+}
+
+function mostrarAlumnos() {
+  const ul = document.getElementById('alumnosList');
+  ul.innerHTML = '';
+  alumnos.forEach((a, i) => {
+    ul.innerHTML += `<li>${a} <button onclick="eliminarAlumno(${i})">Eliminar</button></li>`;
+  });
+}
+
+function eliminarAlumno(index) {
+  alumnos.splice(index, 1);
+  mostrarAlumnos();
+}
+
